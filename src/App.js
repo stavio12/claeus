05a3603a4964095585dd5a506026faed27000196
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2021-03-17 07:45:35 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Form, Button } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Container>
+          <h1 className="pt-5 mt-5">CLAEUS IS COMING SOON</h1>
+        </Container>
+      </main>
     </div>
   );
 }
